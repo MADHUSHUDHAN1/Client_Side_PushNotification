39810a705eaf202083745b084511fd6c5ef6fb23
NorthEastKnow <madhushudhanmondal1234@gmail.com>
2022-07-25 00:17:04 +0530

    schema model change

--- client/model/schema.js
+++ client/model/schema.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    subscription: Object,
+    endpoint: Object,
     // expirationTime: Boolean,
     // p256dh: String,
     // auth: String,
